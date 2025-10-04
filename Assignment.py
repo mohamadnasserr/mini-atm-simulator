4ec51e9816b08cadc416ae8d3212513d9a86fb8f
Muhammad Nasser <mohamad.naser59@gmail.com>
2025-10-05 02:52:55 +0300

    Completed the code

--- Assignment.py
+++ Assignment.py
@@ -1,8 +1,8 @@
-balance = 1000
+balance = 1000.0
 
 print("Welcome to the ATM")
 print("Your balance is " , balance)
-print("Below is the ATM actions  ")
+print("Below is the ATM options  ")
 
 print("1. Check Balance")
 print("2. Deposit Money")
@@ -10,7 +10,25 @@ print("3. Withdraw Money")
 print("4. Exit")
 
 while True:
-    option=int(input("Enter your guess : "))
-    if option ==4 :
+    option=int(input("Choose your option: "))
+    if option == 1 :
+        print("Available balance is :", balance)
+        print("")
+    elif option == 2:
+        deposit_amount=float(input("Enter amount to deposit : "))
+        balance = balance + deposit_amount
+        print("Deposit successful. New balanace = ", balance)
+        print("")
+    elif option == 3 :
+        withdraw_amount=float(input("Enter amount to withdraw : "))
+        if withdraw_amount > balance:
+            print("Insufficient funds!")
+            print("")
+        else : 
+            balance = balance - withdraw_amount
+            print("Withdrawal successful. New balance = ", balance)
+            print("")
+
+    elif option == 4 :
         print("Goodbye")
         break
